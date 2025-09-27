02170e8d69d11068cbe030f3655a34d17f1fc111
MARCOS ANTONIO SOARES DE ANDRADE FILHO <marcos.de.andrade@gmail.com>
2025-09-27 11:37:38 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,11 @@ import './globals.css'
 
 const HEAD_SCRIPTS = process.env.HEAD_SCRIPTS as string
 
-const APP_NAME = 'Gemini Next Chat'
-const APP_DEFAULT_TITLE = 'Gemini Next Chat'
+const APP_NAME = 'Gemini Pembroke'
+const APP_DEFAULT_TITLE = 'Gemini Pembroke'
 const APP_TITLE_TEMPLATE = '%s - PWA App'
 const APP_DESCRIPTION =
-  'Deploy your private Gemini application for free with one click, supporting Gemini 1.5 and Gemini 2.0 models. 一键免费部署您的私人 Gemini 应用, 支持 Gemini 1.5 和 Gemini 2.0 模型。'
+  'Seu Gemini com acesso aos mais diversos modelos da poderosa IA do Google.'
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
